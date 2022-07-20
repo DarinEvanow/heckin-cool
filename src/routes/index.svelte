501f25d2a13463a20440ca94ce5227435f86d0d8
Darin Evanow <darin.evanow@gusto.com>
2022-07-20 13:59:40 -0700

    Migrate from svelte-cubed to threlte.

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,27 +1,61 @@
 <script>
-	import * as THREE from 'three';
-	import * as SC from 'svelte-cubed';
+	import { CircleBufferGeometry, MeshStandardMaterial, BoxBufferGeometry, DoubleSide } from 'three';
+	import { DEG2RAD } from 'three/src/math/MathUtils';
+	import {
+		AmbientLight,
+		Canvas,
+		DirectionalLight,
+		Group,
+		Mesh,
+		OrbitControls,
+		PerspectiveCamera
+	} from '@threlte/core';
+	import { spring } from 'svelte/motion';
+
+	const scale = spring(1);
 </script>
 
-<SC.Canvas antialias background={new THREE.Color('grey')}>
-	<SC.Mesh
-		geometry={new THREE.BoxGeometry()}
-		material={new THREE.MeshStandardMaterial({ color: 'purple' })}
-	/>
-	<SC.PerspectiveCamera position={[5, 5, 5]} />
-	<SC.AmbientLight intensity={0.6} />
-	<SC.DirectionalLight intensity={0.6} position={[-2, 3, 2]} />
+<div>
+	<Canvas>
+		<PerspectiveCamera position={{ x: 10, y: 10, z: 10 }} fov={24}>
+			<OrbitControls
+				maxPolarAngle={DEG2RAD * 80}
+				autoRotate={false}
+				enableZoom={false}
+				target={{ y: 0.5 }}
+			/>
+		</PerspectiveCamera>
 
-	<SC.Group position={[0, -0.5, 0]}>
-		<SC.Mesh
-			geometry={new THREE.PlaneGeometry(50, 50)}
-			material={new THREE.MeshStandardMaterial({ color: 'burlywood' })}
-			rotation={[-Math.PI / 2, 0, 0]}
-		/>
+		<DirectionalLight shadow position={{ x: 3, y: 10, z: 10 }} />
+		<DirectionalLight position={{ x: -3, y: 10, z: -10 }} intensity={0.2} />
+		<AmbientLight intensity={0.2} />
 
-		<SC.Primitive
-			object={new THREE.GridHelper(50, 50, 0x444444, 0x555555)}
-			position={[0, 0.001, 0]}
+		<!-- Cube -->
+		<Group scale={$scale}>
+			<Mesh
+				interactive
+				on:pointerenter={() => ($scale = 2)}
+				on:pointerleave={() => ($scale = 1)}
+				position={{ y: 0.5 }}
+				castShadow
+				geometry={new BoxBufferGeometry(1, 1, 1)}
+				material={new MeshStandardMaterial({ color: '#333333' })}
+			/>
+		</Group>
+
+		<!-- Floor -->
+		<Mesh
+			receiveShadow
+			rotation={{ x: -90 * (Math.PI / 180) }}
+			geometry={new CircleBufferGeometry(3, 72)}
+			material={new MeshStandardMaterial({ side: DoubleSide, color: 'white' })}
 		/>
-	</SC.Group>
-</SC.Canvas>
+	</Canvas>
+</div>
+
+<style>
+	div {
+		height: 100%;
+		width: 100%;
+	}
+</style>
