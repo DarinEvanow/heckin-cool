7d869290648d2b382ade12987ba8a02748fcf0bd
Darin Evanow <darin.evanow@gusto.com>
2022-07-27 22:56:46 -0700

    Add an intro animation to zoom into the logo.

--- src/lib/LandingPage.svelte
+++ src/lib/LandingPage.svelte
@@ -1,9 +1,10 @@
 <script lang="ts">
-	import { MeshMatcapMaterial, TextureLoader, TorusGeometry } from 'three';
+	import { MeshMatcapMaterial, TextureLoader, TorusGeometry, Vector3 } from 'three';
 	import { FontLoader } from 'three/examples/jsm/loaders/FontLoader';
 	import { TextGeometry } from 'three/examples/jsm/geometries/TextGeometry';
 	import { Mesh, PerspectiveCamera, OrbitControls } from '@threlte/core';
 	import { Float } from '@threlte/extras';
+	import { cameraPosition } from '$lib/cameraStore';
 
 	/**
 	 * Textures
@@ -38,22 +39,23 @@
 
 	const material = new MeshMatcapMaterial({ matcap: matcapTexture });
 	const donutGeometry = new TorusGeometry(0.3, 0.2, 20, 45);
+	cameraPosition.set(new Vector3(5, 5, 15));
 </script>
 
-<PerspectiveCamera position={{ x: 5, y: 5, z: 15 }} fov={24} lookAt={{ x: 0, y: 0, z: 0 }}>
+<PerspectiveCamera position={$cameraPosition} fov={24} lookAt={{ x: 0, y: 0, z: 0 }}>
 	<OrbitControls enableZoom={false} />
 </PerspectiveCamera>
 
 <Mesh geometry={textGeometry} {material} />
-{#each Array(150) as _}
+{#each Array(300) as _}
 	<Float speed={2}>
 		<Mesh
 			geometry={donutGeometry}
 			{material}
 			position={{
-				x: (Math.random() - 0.5) * 13,
-				y: (Math.random() - 0.5) * 13,
-				z: (Math.random() - 0.5) * 13
+				x: (Math.random() - 0.5) * 20,
+				y: (Math.random() - 0.5) * 20,
+				z: (Math.random() - 0.5) * 20
 			}}
 			rotation={{ x: Math.random() * Math.PI, y: Math.random() * Math.PI }}
 			scale={Math.random()}
